831ce5caba619d03f959200df692a572bf9a6afd
TylerAngelier <tylerangelier@gmail.com>
2025-07-12 14:56:33 -0400

    fix: ignore .aider* files in git and add author comment to SchedulerApplication.

--- src/main/java/dev/trangelier/scheduler/SchedulerApplication.java
+++ src/main/java/dev/trangelier/scheduler/SchedulerApplication.java
@@ -4,6 +4,11 @@ import dev.trangelier.scheduler.jobdetail.QuartzJobDetailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main application class that starts the scheduler.
+ *
+ * @author Tyler Angelier
+ */
 @SpringBootApplication
 public class SchedulerApplication {
 
